f3cab6420f8382edddfd60ee7857b1adc7ce1c25
nickmon7 <nickmon@uw.edu>
2016-04-22 21:30:05 -0700

    Delete run_rouge.sh

--- run_rouge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#This script runs ROUGE for all four settings four our summaries
-
-CONFIGFILE="/workspace/ling573_sp_2016/nickmon_calderma_kwlabuda/src/configFile.xml"
-
-perl /dropbox/15-16/573/code/ROUGE/ROUGE-1.5.5.pl -e /dropbox/15-16/573/code/ROUGE/data -a -n 4 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d $CONFIGFILE > /workspace/ling573_sp_2016/nickmon_calderma_kwlabuda/results/D2_rouge_scores.out
