7e280599ac0fb227bf515956319beb65d2d031ba
nickmon7 <nickmon@uw.edu>
2016-04-22 21:13:24 -0700

    Added files via upload

--- src/run_rouge.sh
+++ src/run_rouge.sh
@@ -4,4 +4,4 @@
 
 CONFIGFILE="/workspace/ling573_sp_2016/nickmon_calderma_kwlabuda/src/configFile.xml"
 
-./opt/dropbox/15-16/573/code/ROUGE/ROUGE-1.5.5.pl -e /opt/dropbox/15-16/573/code/ROUGE/data -a -n 1 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d $CONFIGFILE
+./dropbox/15-16/573/code/ROUGE/ROUGE-1.5.5.pl -e /dropbox/15-16/573/code/ROUGE/data -a -n 1 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d $CONFIGFILE
