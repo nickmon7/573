3c1fcbab919cfa092750a98adc0372138389784f
nickmon7 <nickmon@uw.edu>
2016-05-15 12:32:14 -0700

    Add files via upload

---
+++ src/D3.cmd
@@ -0,0 +1,10 @@
+Executable = /workspace/ling573_sp_2016/nickmon_calderma_kwlabuda/src/d3.sh
+Universe   = vanilla
+getenv     = true
+output     = condorLog/d3.out
+error      = condorLog/d3.err
+Log        = condorLog/d3.log
+transfer_executable = false
+request_memory = 2*1024
+notification = never
+Queue
---
+++ src/d3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#This is our comprehensive script for the deliverables 2 - it runs our system end to end.
+
+#Step 1 - preprocess the files
+
+mono preprocess.exe
+
+#Step 2 - make summaries
+
+python content_selection.py
+
+#Step 3 - Evaluate the summaries using ROUGE
+
+./run_rouge.sh
